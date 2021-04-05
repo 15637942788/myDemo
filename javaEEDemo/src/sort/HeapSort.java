8bc4a06e336cafa4bc2373cc30037a62ca1aeb84
15637942788 <15637942788@163.com>
2021-04-05 20:51:55 +0800

    排序：堆排的代码分析

--- javaEEDemo/src/sort/HeapSort.java
+++ javaEEDemo/src/sort/HeapSort.java
@@ -35,14 +35,14 @@ public class HeapSort {
     }
 
     //构造大根堆
-    public static void heapInsert(int[] arr) {
+    public static void heapInsert(int[] arr) {//这里保证了第0位是最大的顶堆
         for (int i = 0; i < arr.length; i++) {
             //当前的索引值
             int currentIndex = i;
             //父索引
             int fatherIndex = (currentIndex - 1) / 2;
             //循环：条件当前节点的值大于父节点的值，则交换，并将索引指向父节点
-            while (arr[currentIndex] > arr[fatherIndex]) {
+            while (arr[currentIndex] > arr[fatherIndex]) {//这点可以理解成当前节点在最下层，需要一步一步的往上层去比较如果比上层大，则换位置，继续和上层比较
                 //交换索引对应元素的值
                 swap(arr, currentIndex, fatherIndex);
                 //当前索引指向父索引
@@ -55,23 +55,24 @@ public class HeapSort {
 
     //构造剩余的大根堆
     public static void heapify(int[] arr, int index, int size) {
+        //这里需要明白的是：构造完大根堆之后 除了顶端的值是不符合大根堆的结构（即父节点大于子节点）之外，其他都符合，所以只要构造最顶端的大根堆就行
         int left = index * 2 + 1;
         int right = index * 2 + 2;
-        while (left < size) {
+        while (left < size) {//如果左节点大于长度，说明没有子节点了，就剩自己一个根节点了，就不需要比较了
             int largesIndex;
-            if (arr[left] < arr[right] && right < size) {
+            if (arr[left] < arr[right] && right < size) {//先比较子节点，左右子节点的大小，找出大的那个节点
                 largesIndex = right;
             } else {
                 largesIndex = left;
             }
-            if (arr[index] > arr[largesIndex]) {
+            if (arr[index] > arr[largesIndex]) {//如果当前节点的值大于大的节点的值，则将当前节点赋值给大节点，目的是为了跳出循环
                 largesIndex = index;
             }
             if (index == largesIndex) {
                 break;
             }
-            swap(arr, largesIndex, index);
-            index = largesIndex;
+            swap(arr, largesIndex, index);//这里将最大节点的值放到当前节点
+            index = largesIndex;//再把最大节点赋值给当前节点，可以理解为继续往下比较，因为，你最后拿上来的这个值，并不能确定大于它的子子节点，所以需要继续比较
             left = index * 2 + 1;
             right = index * 2 + 2;
         }
